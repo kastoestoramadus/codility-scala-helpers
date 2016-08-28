ea66079cc85a804fa9a3119ac2da68b7fb324dab
Waldemar Wosiński <kastoestoramadus@gmail.com>
2016-08-28 14:50:53 +0200

    fix, FYI: codility do not use JDK8 for scala

--- src/main/scala/com/codility/lessons/CountDiv.scala
+++ src/main/scala/com/codility/lessons/CountDiv.scala
@@ -5,15 +5,11 @@ import com.codility.Solution
 object CountDiv extends Solution[(Int, Int, Int), Int]{
 
   override def solution(tuple: (Int, Int, Int)): Int = { val (a, b, k) = tuple
-    val p = (if(a==0) k - 1 else a - 1 ) % k
-    val l = b - a + 1 + p
-    l/k
+    val p =  java.lang.Math.floorMod(a - 1, k)
+    val l = (b - a + 1).toLong + p
+    (l/k).toInt
   }
 }
-
-
-
-
 
 
 
