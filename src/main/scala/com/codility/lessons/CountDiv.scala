e68bd0a96067068f28b3d226592b9e570ddcfc3e
Waldemar Wosiński <kastoestoramadus@gmail.com>
2016-08-28 14:14:22 +0200

    fix

--- src/main/scala/com/codility/lessons/CountDiv.scala
+++ src/main/scala/com/codility/lessons/CountDiv.scala
@@ -5,7 +5,7 @@ import com.codility.Solution
 object CountDiv extends Solution[(Int, Int, Int), Int]{
 
   override def solution(tuple: (Int, Int, Int)): Int = { val (a, b, k) = tuple
-    val p = (a-1)%k
+    val p = (if(a==0) k - 1 else a - 1 ) % k
     val l = b - a + 1 + p
     l/k
   }
